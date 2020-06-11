746c87fa6f03521ef3fcf90389c2219a7672c93c
zwbetz-gh <zwbetz@gmail.com>
2020-06-10 22:25:23 -0500

    update count on event

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,11 @@
 (function () {
+  function updateCount(count) {
+    var listCount = document.getElementById("list-count");
+    listCount.innerText = "Count: " + count;
+  }
+
   function onEvent() {
+    var count = 0;
     var filter = search.value.toUpperCase();
     var list = document.getElementById("list");
     var listItems = list.getElementsByTagName("li");
@@ -8,11 +14,14 @@
       var text = item.innerText.toUpperCase();
       if (text.indexOf(filter) > -1) {
         item.style.display = "";
+        count++;
       } else {
         item.style.display = "none";
       }
     }
+    updateCount(count);
   }
+
   var search = document.getElementById("search");
   if (search) {
     search.addEventListener("keyup", onEvent);
